ebdf7006e01ae0aa80efa13b592a5e3a3f9161a2
Marco Wessel <marco@poop.nl>
2014-06-10 19:35:47 +0200

    Support publishing repos

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -10,26 +10,61 @@
 #   Specify which component to put the package in. This option will only works
 #   for aptly version >= 0.5.0.
 #
-define aptly::repo(
+define aptly::repo (
   $component = '',
+  $distribution = '',
+  $prefix = '.',
+  $publish = false,
+  $keyring = '',
+  $secring = '',
 ){
   validate_string($component)
 
-  $aptly_cmd = '/usr/bin/aptly repo'
+  $aptly_cmd = '/usr/bin/aptly'
 
   if empty($component) {
     $component_arg = ''
   } else{
-    $component_arg = "-component=\"${component}\""
+    $component_arg = "-component='${component}'"
   }
 
-
-  exec{ "aptly_repo_create-${title}":
-    command => "${aptly_cmd} create ${component_arg} ${title}",
-    unless  => "${aptly_cmd} show ${title} >/dev/null",
+  exec {"aptly_repo_create-${title}":
+    command => "${aptly_cmd} repo create ${component_arg} ${title}",
+    unless  => "${aptly_cmd} repo show ${title} >/dev/null",
     user    => 'root',
     require => [
       Class['aptly'],
     ],
   }
+
+  if $publish {
+    if empty($distribution){
+      $distribution_arg = ''
+    } else {
+      $distribution_arg = "-distribution='${distribution}'"
+    }
+
+    if empty($keyring){
+      $keyring_arg = ''
+    } else {
+      $keyring_arg = "-keyring='${keyring}'"
+    }
+
+    if empty($secring){
+      $secring_arg = ''
+    } else {
+      $secring_arg = "-secret-keyring='${secring}'"
+    }
+
+    exec {"aptly_publish_repo-${title}":
+      command => "${aptly_cmd} publish ${keyring_arg} ${secring_arg} ${distribution_arg} repo ${title} ${prefix}",
+      unless  => "${aptly_cmd} publish list -raw | grep '${prefix} ${distribution}'",
+      user    => 'root',
+      require => [
+        Class['aptly'],
+        File[$keyring, $secring],
+      ]
+    }
+  }
+
 }
